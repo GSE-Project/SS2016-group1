a3cc2280670937f77bb85dcabce82ff01fb4999e
MBaniasad <mb.sepanta@gmail.com>
2016-05-18 15:15:04 +0200

    #28 done with refactoring current code this effects:line#18 bus#21 and route#17

--- buergerbusserver/core/src/main/java/gse1/buergerbusserver/linemanagement/logic/impl/LinemanagementImpl.java
+++ buergerbusserver/core/src/main/java/gse1/buergerbusserver/linemanagement/logic/impl/LinemanagementImpl.java
@@ -9,9 +9,14 @@ import javax.transaction.Transactional;
 import org.springframework.stereotype.Component;
 
 import gse1.buergerbusserver.general.logic.base.AbstractComponentFacade;
+import gse1.buergerbusserver.linemanagement.dataaccess.api.BusEntity;
+import gse1.buergerbusserver.linemanagement.dataaccess.api.dao.BusDao;
 import gse1.buergerbusserver.linemanagement.dataaccess.api.dao.LineDao;
+import gse1.buergerbusserver.linemanagement.dataaccess.api.dao.RouteDao;
 import gse1.buergerbusserver.linemanagement.logic.api.Linemanagement;
+import gse1.buergerbusserver.linemanagement.logic.api.to.BusEto;
 import gse1.buergerbusserver.linemanagement.logic.api.to.LineEto;
+import gse1.buergerbusserver.linemanagement.logic.api.to.RouteEto;
 
 /**
  * @author razadfki
@@ -25,6 +30,12 @@ public class LinemanagementImpl extends AbstractComponentFacade implements Linem
   @Inject
   private LineDao lineDao;
 
+  @Inject
+  private BusDao busDao;
+
+  @Inject
+  private RouteDao routeDao;
+  
   @Override
   public List<LineEto> getAllLines() {
 
@@ -38,4 +49,30 @@ public class LinemanagementImpl extends AbstractComponentFacade implements Linem
 
   }
 
+  @Override
+  public List<BusEto> findAllBuses() {
+
+    // List<BusEntity> buses = this.busDao.findAll();
+    // return getBeanMapper().mapList(buses, BusEto.class);
+    try {
+      return getBeanMapper().mapList(this.busDao.findAll(), BusEto.class);
+    } catch (Exception e) {
+      System.out.println("Error: " + e.getMessage());
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  @Override
+  public List<BusEto> findBusesOnLine(Long lineId) {
+
+    List<BusEntity> buses = this.busDao.getBusesOnLine(lineId);
+    return getBeanMapper().mapList(buses, BusEto.class);
+  }
+
+  @Override
+  public List<RouteEto> getAllRoutes() {
+
+    return getBeanMapper().mapList(this.routeDao.findAll(), RouteEto.class);
+  }
 }
--- buergerbusserver/core/src/main/java/gse1/buergerbusserver/linemanagement/service/impl/rest/LinemanagementRestServiceImpl.java
+++ buergerbusserver/core/src/main/java/gse1/buergerbusserver/linemanagement/service/impl/rest/LinemanagementRestServiceImpl.java
@@ -4,9 +4,14 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.PathParam;
 
 import gse1.buergerbusserver.linemanagement.logic.api.Linemanagement;
+import gse1.buergerbusserver.linemanagement.logic.api.to.BusEto;
 import gse1.buergerbusserver.linemanagement.logic.api.to.LineEto;
+import gse1.buergerbusserver.linemanagement.logic.api.to.RouteEto;
 import gse1.buergerbusserver.linemanagement.service.api.rest.LinemanagementRestService;
 
 /**
@@ -30,4 +35,39 @@ public class LinemanagementRestServiceImpl implements LinemanagementRestService 
     }
   }
 
+  @Override
+  public List<BusEto> listAllBuses() {
+
+    try {
+      return this.linemanagement.findAllBuses();
+    } catch (Exception e) {
+      System.out.println("Exception:" + e.getMessage());
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  @Override
+  public List<BusEto> listBusesOnLine(@PathParam("lineId") String lineId) {
+
+    long lineIdLong;
+    if (lineId == null) {
+      throw new BadRequestException("missing line id");
+    }
+    try {
+      lineIdLong = Long.parseLong(lineId);
+    } catch (NumberFormatException e) {
+      throw new BadRequestException("Line id is not a number");
+    } catch (NotFoundException e) {
+      throw new BadRequestException("Line not found");
+    }
+    List<BusEto> lineBuses = this.linemanagement.findBusesOnLine(lineIdLong);
+    return lineBuses;
+  }
+  
+  @Override
+  public List<RouteEto> getAllRoutes() {
+
+    return this.linemanagement.getAllRoutes();
+  }
 }
