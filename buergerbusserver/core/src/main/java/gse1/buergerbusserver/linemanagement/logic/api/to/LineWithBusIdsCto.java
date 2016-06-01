7bc04ebfadfd72494c85ada48a289120d4c27233
MBaniasad <mb.sepanta@gmail.com>
2016-06-01 16:56:49 +0200

    #48 "busids" instead of "busses"

--- buergerbusserver/core/src/main/java/gse1/buergerbusserver/linemanagement/logic/api/to/LineWithBusIdsCto.java
+++ buergerbusserver/core/src/main/java/gse1/buergerbusserver/linemanagement/logic/api/to/LineWithBusIdsCto.java
@@ -8,23 +8,23 @@ import java.util.List;
 
 public class LineWithBusIdsCto extends LineEto {
 
-  
+
   private List<BusEto> buses;
-  public List<Long> getBusids() {
+  public List<Long> getBusses() {
 
     List<Long> returnlist = new ArrayList<Long>();
-    
+
     for(BusEto bus:this.buses)
       returnlist.add(bus.getId());
     return returnlist;
   }
 
-  
+
 
   public void setBuses(List<BusEto> buses) {
 
     this.buses = buses;
-    
+
   }
 
 }
