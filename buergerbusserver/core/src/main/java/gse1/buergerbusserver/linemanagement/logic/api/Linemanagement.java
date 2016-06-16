1bc77ca0f01c4d958c6d0089fd7b1f7dcae2e0d1
Jay <bonkilejay@gmail.com>
2016-06-16 16:09:00 +0200

    #64 Commitng LineManagement

--- buergerbusserver/core/src/main/java/gse1/buergerbusserver/linemanagement/logic/api/Linemanagement.java
+++ buergerbusserver/core/src/main/java/gse1/buergerbusserver/linemanagement/logic/api/Linemanagement.java
@@ -4,15 +4,17 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
+import gse1.buergerbusserver.linemanagement.common.api.CustomStop;
 import gse1.buergerbusserver.linemanagement.common.api.Line;
 import gse1.buergerbusserver.linemanagement.dataaccess.api.BusEntity;
+import gse1.buergerbusserver.linemanagement.dataaccess.api.CustomStopEntity;
 import gse1.buergerbusserver.linemanagement.logic.api.to.BusEto;
 import gse1.buergerbusserver.linemanagement.logic.api.to.LastPositionEto;
 import gse1.buergerbusserver.linemanagement.logic.api.to.LineEto;
 import gse1.buergerbusserver.linemanagement.logic.api.to.RouteEto;
 
 /**
- * @author razadfki
+ * @author JAYU
  *
  */
 
@@ -89,4 +91,73 @@ public interface Linemanagement {
    */
   void setLastPosition(Long busId, double lon, double lat);
 
+  /**
+   * Returns a list of all {@link CustomStopEntity} pending {@link CustomStop} as given by requestId and deviceId (For
+   * Citizen)
+   *
+   * @param requestId
+   * @param deviceId
+   * @return {@link List} of Custom Stop requests pending for the request ID and device ID
+   **/
+  List<CustomStopEntity> getCustomStopStatus(Long requestId, String deviceId);
+
+  /**
+   * Returns a list of all {@link CustomStopEntity} pending {@link CustomStop} as given by deviceId (For Citizen)
+   *
+   * @param deviceId
+   * @return {@link List} of Custom Stop requests pending for the device ID
+   **/
+  List<CustomStopEntity> getCustomStopDevice(String deviceId);
+
+  /**
+   * Returns a list of all {@link CustomStopEntity} pending {@link Line} as given by lineId (For Driver)
+   *
+   * @param lineId
+   * @return {@link List} of Custom Stop requests pending for the device ID
+   **/
+  List<CustomStopEntity> getCustomStopLine(Long lineId);
+
+  /**
+   * Returns a list of all {@link CustomStopEntity} for {@link CustomStop} as given by Status (For Driver/Citizen)
+   *
+   * @param status
+   * @return {@link List} of Custom Stop requests in that status (1 - Accepted, 2 - Declined, 3 - Pending, 4 -
+   *         Completed, 5 - Cancelled) Not really needed right now...
+   **/
+  List<CustomStopEntity> getCustomStopRequests(int status);
+
+  /**
+   * Update the status of the {@link CustomStopEntity} request for {@link CustomStop} as given by request ID
+   *
+   * @param status
+   * @param requestId
+   **/
+  void updateCustomStopStatus(Long requestId, int status);
+
+  /**
+   * Update the status of the {@link CustomStopEntity} request for {@link CustomStop} as given by request ID
+   *
+   * @param lineId LineId reference of the Line
+   * @param pickUpTime PickUpTime from the Custom Stop
+   * @param lat Latitude of the Custom Stop
+   * @param lon Longitude of the Custom Stop
+   * @param numberOfPersons Number of Persons requested the custom Stop
+   * @param deviceId devideId of the Person who requested the custom Stop
+   * @param userName Unique name of the person requesting custom stop
+   * @param userAddress User address of the person requesting custom stop
+   * @param userAssistance List of User assistance available
+   *
+   * @return {@link List} of Custom Stop requests pending for the device ID
+   **/
+  public List<CustomStopEntity> newCustomStop(Long lineId, Date pickUpTime, double lat, double lon, int numberOfPersons,
+      String deviceId, String userName, String userAddress, List<Integer> userAssistance);
+
+  /**
+   * get the last updated timeStamp in {@link CustomStopEntity}
+   *
+   * @return Date
+   */
+  Date lastUpdate();
+
 }
+
