9ece2c4d56475be9028639cb427d119ed3e95dcd
gse01 <ricarda42@users.noreply.github.com>
2016-06-27 11:30:57 +0200

    bug fixed empty assistance throws error, fixes #83

--- buergerbusserver/core/src/main/java/gse1/buergerbusserver/general/common/api/datatype/InfoObject.java
+++ buergerbusserver/core/src/main/java/gse1/buergerbusserver/general/common/api/datatype/InfoObject.java
@@ -5,35 +5,43 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InfoObject {
-	
-	  private String name;
-	  private String address;
-	  private List<Integer> assistance;
-	  
-	  public InfoObject(String name, String address,String assist){
-	    
-	    List<String> stringList = Arrays.asList(assist.split(","));
-	    List<Integer> returnList = new ArrayList<>();
-	    for (String num : stringList) {
-	      returnList.add(Integer.valueOf(num));
-	    }
-	    
-	    this.name = name;
-	    this.address = address;
-	    this.assistance = returnList;
-	   }
 
-	 
-	  public String getName()
-	  {
-	    return this.name;
-	  }
-	  public String getAddress()
-	  {
-	    return this.address;
-	  }
-	  public List<Integer> getAssistance()
-	  {
-	    return this.assistance;
-	  }
+  private String name;
+
+  private String address;
+
+  private List<Integer> assistance;
+
+  public InfoObject(String name, String address, String assist) {
+
+    if (!assist.isEmpty()) {
+      List<String> stringList = Arrays.asList(assist.split(","));
+      List<Integer> returnList = new ArrayList<>();
+      for (String num : stringList) {
+        returnList.add(Integer.valueOf(num));
+      }
+      this.assistance = returnList;
+    } else {
+      this.assistance = new ArrayList<>();
+    }
+
+    this.name = name;
+    this.address = address;
+
+  }
+
+  public String getName() {
+
+    return this.name;
+  }
+
+  public String getAddress() {
+
+    return this.address;
+  }
+
+  public List<Integer> getAssistance() {
+
+    return this.assistance;
+  }
 }
